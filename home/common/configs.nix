9c6fc63d90c0ff9d2a37a795c44223f163fdcd77
Donald Gifford <dgifford@pm.me>
2026-07-22 07:56:11 -0400

    herdr

--- home/common/configs.nix
+++ home/common/configs.nix
@@ -32,5 +32,6 @@ in
     "1Password/ssh/agent.toml".source = link "1Password/ssh/agent.toml";
     "mise/conf.d/global.toml".source = link "mise/conf.d/global.toml";
     "herdr/config.toml".source = link "herdr/config.toml";
+    "herdr/scripts/agent-picker.sh".source = link "herdr/scripts/agent-picker.sh";
   };
 }
